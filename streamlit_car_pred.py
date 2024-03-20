3f6564106b8a1e44e00adbd27edb65e22b7ed10f
Victor7615 <92019004+Victor7615@users.noreply.github.com>
2024-03-20 05:13:13 +0100

    Update streamlit_car_pred.py

--- streamlit_car_pred.py
+++ streamlit_car_pred.py
@@ -6,12 +6,7 @@ import joblib
 #model =pickle.load(open('C:\\Users\\USER\\Desktop\\Machine_learning_end_to_end\\random_forest_carprice_model.pkl','rb'))  #if you are using pickle not joblib
 model_path='C:\\Users\\USER\\Desktop\\Machine_learning_end_to_end\\random_forest_carprice_model1.pkl'
 model = joblib.load(model_path)
-#try:
-#    model = joblib.load(model_path)
-#    print("Model loaded successfully!")
-#except FileNotFoundError:
-#    print(f"Model not found at {model_path}. Please ensure the file exists.")
-#    exit(1)
+
 
 def main():
     st.title("Car pricing Prediction Solution")
@@ -19,15 +14,16 @@ def main():
 
     # creating input Variables
 
-    Year =st.number_input('Year',step=1,min_value=1990,max_value=2024)
-    Present_Price=st.number_input('Present_Price',step=1)
-    Age_of_Car=st.number_input('Age_of_Car',step=1)
-    Kms=st.number_input('Kms_Driven',step=1)
-    Owner=st.selectbox('Owner',(1,0))
-    Fuel_Type_Diesel=st.selectbox('Fuel_Type_Diesel',(1,0))
-    Fuel_Type_Petrol=st.selectbox('Fuel_Type_Petrol',(1,0))
-    Seller_Type_Individual=st.selectbox('Seller_Type_Individual',(1,0))
-    Transmission_Manual=st.selectbox('Transmission_Manual',(1,0))
+    Year = st.number_input('Year', step=1, min_value=1990, max_value=2024)
+    Present_Price = st.number_input('Present_Price', step=1)
+    Age_of_Car = st.number_input('Age_of_Car', step=1)
+    Kms = st.number_input('Kms_Driven', step=1)
+    Owner = 1 if st.selectbox('Owner', ('First Owner', 'More Than One Previous Owners')) == 'First Owner' else 0
+    Fuel_Type_Diesel = 1 if st.selectbox('Fuel_Type_Diesel', ('Yes', 'No')) == 'Yes' else 0
+    Fuel_Type_Petrol = 1 if st.selectbox('Fuel_Type_Petrol', ('Yes', 'No')) == 'Yes' else 0
+    Seller_Type_Individual = 1 if st.selectbox('Seller_Type_Individual', ('Yes', 'No')) == 'Yes' else 0
+    Transmission_Manual = 1 if st.selectbox('Transmission_Manual', ('Manual', 'Automatic')) == 'Manual' else 0
+
 
     # prediction
     if st.button('Predict'):
